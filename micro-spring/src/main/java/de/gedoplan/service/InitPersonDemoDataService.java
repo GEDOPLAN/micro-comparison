38acd25e171515721548e73942a6760ad574fd4d
Dirk Weil <dirk.weil@gedoplan.de>
2019-05-22 22:26:29 +0200

    Testdaten-Init ergänzt

---
+++ micro-spring/src/main/java/de/gedoplan/service/InitPersonDemoDataService.java
@@ -0,0 +1,36 @@
+package de.gedoplan.service;
+
+import de.gedoplan.entity.Person;
+import de.gedoplan.persistence.PersonRepository;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitPersonDemoDataService {
+
+  private static Log log = LogFactory.getLog(InitPersonDemoDataService.class);
+
+  /**
+   * Create test/demo data.
+   *
+   * @param personRepository
+   *          repository
+   */
+  @Bean
+  CommandLineRunner createDemoData(PersonRepository personRepository) {
+    return args -> {
+      try {
+        if (personRepository.count() == 0) {
+          personRepository.save(new Person("Duck", "Dagobert"));
+          personRepository.save(new Person("Duck", "Donald"));
+        }
+      } catch (Exception e) {
+        log.warn("Cannot create demo data", e);
+      }
+    };
+  }
+}
