cb97b5ed4ae3dfb98d4a799ed7bf2af136f3e076
Dirk Weil <dirk.weil@gedoplan.de>
2019-06-06 14:55:46 +0200

    Thorntail-Version entfernt

--- micro-comparison-thorntail/src/main/java/de/gedoplan/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package de.gedoplan.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rs")
-public class RestApplication extends Application {
-
-}
