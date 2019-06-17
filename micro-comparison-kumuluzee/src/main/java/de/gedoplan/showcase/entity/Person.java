b3d6816d54b161390d1e9e93038c1efcfab2f36c
Dirk Weil <dirk.weil@gedoplan.de>
2019-06-17 23:14:20 +0200

    JAXB-Runtime raus

--- micro-comparison-kumuluzee/src/main/java/de/gedoplan/showcase/entity/Person.java
+++ micro-comparison-kumuluzee/src/main/java/de/gedoplan/showcase/entity/Person.java
@@ -5,9 +5,6 @@ import de.gedoplan.baselibs.persistence.entity.GeneratedIntegerIdEntity;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -16,8 +13,6 @@ import lombok.Setter;
 
 @Entity
 @Access(AccessType.FIELD)
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter
 @Setter
