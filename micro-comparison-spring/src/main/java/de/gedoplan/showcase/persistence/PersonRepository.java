7eac67dc91eb2691ec9dd55b8b4b42f6e2e78f9f
Dirk Weil <dirk.weil@gedoplan.de>
2019-06-27 10:41:36 +0200

    Demo für customized Finder ergänzt

--- micro-comparison-spring/src/main/java/de/gedoplan/showcase/persistence/PersonRepository.java
+++ micro-comparison-spring/src/main/java/de/gedoplan/showcase/persistence/PersonRepository.java
@@ -2,6 +2,8 @@ package de.gedoplan.showcase.persistence;
 
 import de.gedoplan.showcase.entity.Person;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
+  // Nur zur Demo - wird in der Anwendung nicht benutzt
+  public List<Person> findByName(String name);
 }
--- micro-comparison-wildfly/src/main/java/de/gedoplan/showcase/persistence/PersonRepository.java
+++ micro-comparison-wildfly/src/main/java/de/gedoplan/showcase/persistence/PersonRepository.java
@@ -2,6 +2,9 @@ package de.gedoplan.showcase.persistence;
 
 import de.gedoplan.baselibs.persistence.repository.SingleIdEntityRepository;
 import de.gedoplan.showcase.entity.Person;
+import de.gedoplan.showcase.entity.Person_;
+
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
@@ -9,4 +12,9 @@ import javax.transaction.Transactional;
 @ApplicationScoped
 @Transactional(rollbackOn = Exception.class)
 public class PersonRepository extends SingleIdEntityRepository<Integer, Person> {
+
+  // Nur zur Demo - wird in der Anwendung nicht benutzt
+  public List<Person> findByName(String name) {
+    return findMultiByProperty(Person_.name, name);
+  }
 }
